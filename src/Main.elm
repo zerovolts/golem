7b4d7543ac47b5ae34fd08b64c44badf0c3c649e
Zach Stone <hexaphixylix@yahoo.com>
2018-05-13 17:41:38 -0400

    add board and stone rendering

--- src/Main.elm
+++ src/Main.elm
@@ -1,32 +1,86 @@
 module Main exposing (..)
 
+import Array exposing (Array)
 import Html exposing (..)
 import Html.Attributes exposing (href, rel)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
 
+stoneRadius : Int
+stoneRadius =
+    12
+
+
+stonePadding : Int
+stonePadding =
+    2
+
+
+stoneSize : Int
+stoneSize =
+    (stoneRadius * 2) + (stonePadding * 2)
+
+
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
 
+type Color
+    = Black
+    | White
+    | Empty
+
+
+type alias GamePiece =
+    { color : Color
+    , x : Int
+    , y : Int
+    }
+
+
+type alias Board =
+    Array (Array Color)
+
+
 type alias Model =
-    Int
+    { board : Board }
 
 
-model : Int
+model : Model
 model =
-    0
+    { board = newBoard 19 }
 
 
 type Msg
-    = Nothing
+    = None
+
+
+newBoard : Int -> Board
+newBoard size =
+    Array.initialize size
+        (\x ->
+            Array.initialize size
+                (\y ->
+                    Black
+                )
+        )
+
+
+placeStone : Color -> Int -> Int -> Board -> Board
+placeStone c x y board =
+    case Array.get x board of
+        Just row ->
+            Array.set y (Array.set x c row) board
+
+        Nothing ->
+            board
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        Nothing ->
+        None ->
             model
 
 
@@ -45,20 +99,15 @@ view model =
             [ stylesheet "main"
             , svg
                 [ class "game-board"
-                , width "320"
-                , height "320"
+                , width (toString (Array.length model.board * stoneSize + (stoneSize * 2)))
+                , height (toString (Array.length model.board * stoneSize + (stoneSize * 2)))
                 ]
-                [ board
-                , stone White 32 32
-                , stone Black 100 100
-                ]
+                ([ drawBoard model.board ]
+                    ++ drawLines model.board
+                    ++ drawStones model.board
+                )
             ]
         ]
-
-
-type Color
-    = Black
-    | White
 
 
 colorToHex : Color -> String
@@ -70,25 +119,92 @@ colorToHex c =
         Black ->
             "#333"
 
+        Empty ->
+            "#000"
 
-stone : Color -> Int -> Int -> Svg Msg
-stone c x y =
+
+
+-- diameter: 24px + 4px padding
+
+
+drawStone : Color -> Int -> Int -> Svg Msg
+drawStone c x y =
     circle
-        [ cx (toString x)
-        , cy (toString y)
-        , r "16"
+        [ cx (toString (x * stoneSize + stoneSize + stoneRadius + 2))
+        , cy (toString (y * stoneSize + stoneSize + stoneRadius + 2))
+        , r (toString stoneRadius)
         , Svg.Attributes.style ("fill: " ++ colorToHex c)
         ]
         []
 
 
-board : Svg Msg
-board =
+drawStones : Board -> List (Svg Msg)
+drawStones board =
+    List.concat
+        (Array.toList
+            (board
+                |> Array.indexedMap
+                    (\i col ->
+                        Array.toList
+                            (Array.indexedMap
+                                (\j piece ->
+                                    drawStone piece i j
+                                )
+                                col
+                            )
+                    )
+            )
+        )
+
+
+drawBoard : Board -> Svg Msg
+drawBoard board =
     rect
         [ x "0"
         , y "0"
-        , width "320"
-        , height "320"
-        , fill "#dc6"
+        , width (toString (Array.length board * stoneSize + (stoneSize * 2)))
+        , height (toString (Array.length board * stoneSize + (stoneSize * 2)))
+        , fill "#dc5"
         ]
         []
+
+
+drawVerticalLine : Board -> Int -> Svg Msg
+drawVerticalLine board y =
+    line
+        [ x1 (toString (stoneSize + stoneRadius + stonePadding))
+        , y1 (toString (y * stoneSize + (stoneSize + stoneRadius + stonePadding)))
+        , x2 (toString (Array.length board * stoneSize + stoneRadius + stonePadding))
+        , y2 (toString (y * stoneSize + (stoneSize + stoneRadius + stonePadding)))
+        , stroke "#333"
+        ]
+        []
+
+
+drawHorizontalLine : Board -> Int -> Svg Msg
+drawHorizontalLine board y =
+    line
+        [ x1 (toString (y * stoneSize + (stoneSize + stoneRadius + stonePadding)))
+        , y1 (toString (stoneSize + stoneRadius + stonePadding))
+        , x2 (toString (y * stoneSize + (stoneSize + stoneRadius + stonePadding)))
+        , y2 (toString (Array.length board * stoneSize + stoneRadius + stonePadding))
+        , stroke "#333"
+        ]
+        []
+
+
+drawLines : Board -> List (Svg Msg)
+drawLines board =
+    (Array.toList (Array.initialize (Array.length board) identity)
+        |> List.map
+            (\x ->
+                drawVerticalLine board x
+            )
+    )
+        ++ (Array.toList
+                (Array.initialize (Array.length board) identity)
+                |> List.map
+                    (\x ->
+                        drawHorizontalLine board x
+                    )
+           )
